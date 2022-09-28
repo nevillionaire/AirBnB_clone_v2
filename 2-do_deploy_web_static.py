f3595ba8a95c04f6f01fc2e5eebf2f66fcb97b83
Nevillionaire <75064256+nevillionaire@users.noreply.github.com>
2022-09-28 18:27:24 +0300

    another one

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-from fabric.api import *
-from datetime import datetime
+"""Deploys static assets"""
+from fabric.api import put, run, env
 import os
 
 env.hosts = ['18.206.194.154', '3.236.115.236']
 
 
-
 def do_deploy(archive_path):
-    """ fabric script to deploy to a server """
-    if not os.path.exists(archive_path):
+    """Deploys static assets"""
+    if not os.path.isfile(archive_path):
         return False
 
-    filename = archive_path.split("/")
-    filename = filename[1]
-    fname = filename.split('.')
-    fname = fname[0]
-
-    newpath = '/data/web_static/releases/{}/'.format(fname)
-
     try:
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(newpath))
-        run("tar -xzf /tmp/{} -C {}".format(filename, newpath))
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
         run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(newpath, newpath))
-        run("rm -rf {}web_static".format(newpath))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(newpath))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
         return True
     except Exception:
         return False
