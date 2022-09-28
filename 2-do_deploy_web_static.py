32e8c0ec7b4662e09570acd83507502b35859ed4
Nevillionaire <75064256+nevillionaire@users.noreply.github.com>
2022-09-28 18:15:20 +0300

    v1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,33 @@
 #!/usr/bin/python3
-# distributes an archive to a web server.
-from fabric.api import put
-from fabric.api import run
-import os.path
-from fabric.api import env
+from fabric.api import *
+from datetime import datetime
+import os
 
-env.hosts = ["ubuntu@18.206.194.154", "ubuntu@3.236.115.236"]
+env.hosts = ['18.206.194.154', '3.236.115.236']
+
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-        Returns false If the file doesn't exist at
-        archive_path or an error occurs
-        Otherwise returns True.
-    """
-    if os.path.isfile(archive_path) is False:
+    """ fabric script to deploy to a server """
+    if not os.path.exists(archive_path):
         return False
 
-    file_name_with_ext = archive_path.split("/")[-1]
-    file_name_no_ext = file_name_with_ext.split(".")[0]
+    filename = archive_path.split("/")
+    filename = filename[1]
+    fname = filename.split('.')
+    fname = fname[0]
 
-    if put(archive_path, "/tmp/{}".format(file_name_with_ext)).failed is True:
+    newpath = '/data/web_static/releases/{}/'.format(fname)
+
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(newpath))
+        run("tar -xzf /tmp/{} -C {}".format(filename, newpath))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(newpath, newpath))
+        run("rm -rf {}web_static".format(newpath))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(newpath))
+        return True
+    except Exception:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(file_name_no_ext)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(file_name_no_ext)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file_name_with_ext, file_name_no_ext)).failed is True:
-        return False
-    if run("rm -r /tmp/{}".format(file_name_with_ext)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/"
-           .format(file_name_no_ext, file_name_no_ext)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(file_name_no_ext)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(file_name_no_ext)).failed is True:
-        return False
-    print("New version deployed!")
-    return True
