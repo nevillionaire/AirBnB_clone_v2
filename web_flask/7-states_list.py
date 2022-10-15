c90a34239c4220c6bebb73c971c80c59204cb9a5
Nevillionaire <75064256+nevillionaire@users.noreply.github.com>
2022-10-15 15:04:36 +0300

    dont run

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
-
+"""
+web application to generate list of states dynamically
+"""
+from flask import Flask, render_template
 from models import storage
-from models.state import State
-from flask import Flask
-from flask import render_template
+from os import getenv
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states():
-    """Returns a rendered html template
-    at the /states_list route,
-    listing all states"""
+    """access File/DB Storage for all State objects and render to HTML"""
+
     return render_template('7-states_list.html',
-                           states=storage.all('State').values())
+                           states=[st for st in storage.all('State').values()])
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
+def teardown_db(exception):
+    """Closes the database again at the end of the request."""
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
